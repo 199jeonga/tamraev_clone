f9a5970832ca82b7cc103de90c1901958fd0bd00
jeonga lee <lja3248@gmail.com>
2022-01-28 23:50:30 +0900

    [feat] react : header menu add transition to button

--- src/component/Header.js
+++ src/component/Header.js
@@ -10,7 +10,11 @@ export default function Header(props) {
     <header id="headBox">
       <div className="head_area">
         <h1><Link to="/"><span className="blind">{props.heading}</span></Link></h1>
-        <button type="button" classname="gnb_btn"><i>메뉴열기</i><span className="blind">메뉴열기</span></button>
+        <div class="gnb_btn">
+          <button type="button">
+            <i className="nav_bar"></i><span className="blind">메뉴열기</span>
+          </button>
+          </div>
       </div>
       <Nvigation />
     </header>
