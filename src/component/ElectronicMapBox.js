227415a38d49eb5e1f557cf817e3e5fb4566276b
jeonga lee <lja3248@gmail.com>
2022-02-11 16:21:34 +0900

    [feat] react : add to kakao map api

--- src/component/ElectronicMapBox.js
+++ src/component/ElectronicMapBox.js
@@ -1,14 +1,39 @@
-import React from 'react'
+/* global kakao */
+import React,{useEffect} from 'react'
 import '../style/electronicMapBox.scss';
+const { kakao } = window;
 
 function ElectronicMapBox() {
+
+  useEffect(() => {
+    let container = document.getElementById("map");
+    (function location(){
+      navigator.geolocation.getCurrentPosition(
+      function(position) {
+      console.log("위도 : " + position.coords.latitude);
+      console.log("경도 : " + position.coords.longitude);
+      }, 
+      );
+                  
+      })();
+    const options = {
+      center: new window.kakao.maps.LatLng(location.coords.latitude, location.coords.longitude),
+      level: 5,
+    };
+
+    let map = new window.kakao.maps.Map(container, options);
+
+    console.log("loading kakaomap");
+  }, []);
+
+
   return (
     <div id="electronicMapBox">
       <span>&#42; 거리순</span>
       <div className="el_map_area">
         <div className="list_inner">
         </div>
-        <div className="map_inner">
+        <div className="map_inner" id="map">
           <div className='map_operating_part'></div>
           <div className='map_zoom_part'></div>
         </div>
