15de32159d0a2b9635d3479ed2a1859f61cd7ee0
jeonga lee <lja3248@gmail.com>
2022-02-12 23:11:48 +0900

    [feat] react : added function to get current location (with, kakao API)

--- src/component/ElectronicMapBox.js
+++ src/component/ElectronicMapBox.js
@@ -6,24 +6,34 @@ const { kakao } = window;
 function ElectronicMapBox() {
 
   useEffect(() => {
-    let container = document.getElementById("map");
-    (function location(){
-      navigator.geolocation.getCurrentPosition(
-      function(position) {
-      console.log("위도 : " + position.coords.latitude);
-      console.log("경도 : " + position.coords.longitude);
-      }, 
-      );
-                  
-      })();
-    const options = {
-      center: new window.kakao.maps.LatLng(location.coords.latitude, location.coords.longitude),
-      level: 5,
-    };
+let container = document.getElementById("map");
+const options = {
+  center: new window.kakao.maps.LatLng(37.56646, 126.98121),
+  level: 5,
+};
+let map = new window.kakao.maps.Map(container, options);
 
-    let map = new window.kakao.maps.Map(container, options);
+function locationLoadSuccess(pos){
+  // 현재 위치 받아오기
+  const currentPos = new kakao.maps.LatLng(pos.coords.latitude,pos.coords.longitude);
+  // 지도 이동(기존 위치와 가깝다면 부드럽게 이동)
+  map.panTo(currentPos);
+  // 마커 생성
+  const marker = new kakao.maps.Marker({
+    position: currentPos
+  });
+  // 기존에 마커가 있다면 제거
+  marker.setMap(null);
+  marker.setMap(map);
+};
 
-    console.log("loading kakaomap");
+const locationLoadError =  ()=>{
+  alert('위치 정보를 가져오는데 실패했습니다.');
+}
+const getCurrent =  ()=>{
+  navigator.geolocation.getCurrentPosition(locationLoadSuccess,locationLoadError);
+}
+  getCurrent();
   }, []);
 
 
