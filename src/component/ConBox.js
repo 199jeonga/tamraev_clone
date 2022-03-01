acf79766c4c61c22aae8c26b9259def1594d42ec
jeonga lee <lja3248@gmail.com>
2022-03-02 00:11:17 +0900

    [petty] react : 메뉴 클릭 시 plus 숫자 리셋

--- src/component/ConBox.js
+++ src/component/ConBox.js
@@ -19,6 +19,7 @@ export default function ConBox() {
   const fnTab = (e, i) => {
     e.preventDefault();
     setNum(i);
+    setPlus(6);
   };
   const fnPlus = () => {
     setPlus(plus + plusN);
