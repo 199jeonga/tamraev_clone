a8a10a79f7208adf5fe62cd34c683a942fd2ab55
jeonga lee <lja3248@gmail.com>
2022-03-02 00:06:23 +0900

    [feat] react : plus button 클릭 시 컨텐츠가 여섯개씩 증가

--- src/component/ConBox.js
+++ src/component/ConBox.js
@@ -1,49 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import { AiOutlineSearch,AiOutlinePlus } from 'react-icons/ai';
-import '../style/conBox.scss';
-import ConBoxContent from './ConBoxContent.js';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { AiOutlineSearch, AiOutlinePlus } from "react-icons/ai";
+import "../style/conBox.scss";
+import ConBoxContent from "./ConBoxContent.js";
 
 export default function ConBox() {
+  const plusN = 6;
 
   const [num, setNum] = useState(0);
   const [conData, setconData] = useState([]);
   const [plus, setPlus] = useState(6);
-  
-  useEffect( ()=>{
-    axios.get('./data/conList.json')
-    .then(res => setconData(res.data))
+
+  useEffect(() => {
+    axios.get("./data/conList.json").then((res) => setconData(res.data));
   }, []);
 
-  const fnTab = (e, i)=>{
+  const fnTab = (e, i) => {
     e.preventDefault();
     setNum(i);
-  }
-  const code = conData.filter( (data, index) => index === num );  
-
-  const fnPlus = ()=> {setPlus(plus+6)}
-  const viewData = code.filter( (data, i)=> i < plus);
+  };
+  const fnPlus = () => {
+    setPlus(plus + plusN);
+  };
+  const code = conData.filter((data, index) => index === num);
+  // console.log(code);
 
   return (
     <div id="conBox">
       <h2>전기차 이용법부터 알찬 꿀팁까지 모두 알려드려요!</h2>
       <div className="con_area">
         <ul className="con_title_inner">
-          { conData.map( (data, idx)=>
-          <li key={idx}><button type="button" onClick={ (e)=>(fnTab(e, idx)) }><span>{data.conTitle}</span></button></li>
-          ) }
-          <li><Link to="/electroniclist" ><span>제주 전기차 충전소 찾기</span></Link></li>
-          <li><Link to="/noticelist" ><span>제주 전기차 뉴스</span></Link></li>
-          <li className="search_btn"><button type="button"><AiOutlineSearch />검색</button></li>
+          {conData.map((data, idx) => (
+            <li key={idx}>
+              <button type="button" onClick={(e) => fnTab(e, idx)}>
+                <span>{data.conTitle}</span>
+              </button>
+            </li>
+          ))}
+          <li>
+            <Link to="/electroniclist">
+              <span>제주 전기차 충전소 찾기</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/noticelist">
+              <span>제주 전기차 뉴스</span>
+            </Link>
+          </li>
+          <li className="search_btn">
+            <button type="button">
+              <AiOutlineSearch />
+              검색
+            </button>
+          </li>
         </ul>
-        <span className='underLine'></span>
+        <span className="underLine"></span>
 
-        {code.map( (data,idx) => <ConBoxContent key={idx} data={data} /> )}
+        {code.map((data, idx) => (
+          <ConBoxContent key={idx} data={data} plus={plus} />
+        ))}
 
-        <div className="more_btn"><button type="button" onClick={fnPlus} ><AiOutlinePlus /></button></div>
+        <div className="more_btn">
+          <button type="button" onClick={fnPlus}>
+            <AiOutlinePlus />
+          </button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
--- src/component/conBoxContent.js
+++ src/component/conBoxContent.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ConBoxDetail from './ConBoxDetail';
+import React from "react";
+import ConBoxDetail from "./ConBoxDetail";
 
-function ConBoxContent({data}) {
+function ConBoxContent({ data, plus }) {
+  const viewData = data.details.filter((el, index) => index < plus);
+  console.log(viewData);
+
   return (
-  <ul className="con_content_inner">
-      {data.details.map((data,i)=> <ConBoxDetail key={i} data={data} />)}
-  </ul>
-  )
+    <ul className="con_content_inner">
+      {viewData.map((data, i) => (
+        <ConBoxDetail key={i} data={data} />
+      ))}
+    </ul>
+  );
 }
 
 export default ConBoxContent;
